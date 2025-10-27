6d14c5bb015dcf6b2b145f3bf4b63ba7d3c21e97
leandro <38253462+lst15@users.noreply.github.com>
2025-10-27 14:24:42 -0300

    Support multiple logins from credential file

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,22 @@
 """Core package for application logic."""
 
 from .client import LoginClient
-from .config import DEFAULT_LOGIN_REQUEST
+from .config import DEFAULT_CREDENTIALS_FILE, DEFAULT_LOGIN_REQUEST
+from .credentials import (
+    CredentialFormatError,
+    DEFAULT_DELIMITER,
+    iter_login_requests,
+    load_credentials,
+)
 from .models import LoginRequest
 
-__all__ = ["LoginClient", "LoginRequest", "DEFAULT_LOGIN_REQUEST"]
+__all__ = [
+    "CredentialFormatError",
+    "DEFAULT_CREDENTIALS_FILE",
+    "DEFAULT_DELIMITER",
+    "DEFAULT_LOGIN_REQUEST",
+    "LoginClient",
+    "LoginRequest",
+    "iter_login_requests",
+    "load_credentials",
+]
